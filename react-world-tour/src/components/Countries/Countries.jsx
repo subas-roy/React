0575ce4d1cf45a60ec1e27210c9900195104ab16
subas-roy <subasroy46@gmail.com>
2024-08-24 22:13:45 +0600

    Display countries in a simple way, Folder Structure

--- react-world-tour/src/components/Countries/Countries.jsx
+++ react-world-tour/src/components/Countries/Countries.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Country from "../Country/Country";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
@@ -6,11 +7,15 @@ const Countries = () => {
   useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => setCountries(data))
   }, [])
+
   return (
     <div>
-      <h3>Countries</h3>
+      <h3>Countries {countries.length}</h3>
+      {
+        countries.map(country => <Country key={country.cca3} country={country}></Country>)
+      }
     </div>
   );
 };
