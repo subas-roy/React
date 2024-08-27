47e8b1eafea0c6efe9017cfe6df5ebca7d95766b
subas-roy <subasroy46@gmail.com>
2024-08-27 07:37:13 +0600

    Create and Load Fake data, chaptgpt json, online api

--- es6-modules-overview/src/App.jsx
+++ es6-modules-overview/src/App.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import Watch from './Components/Watch/Watch'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // way 3: load data from GitHub
+  // declare state
+  const [watches, setWatches] = useState([])
+  // load data
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/subas-roy/watches-data/main/watches.json')
+    .then(res => res.json())
+    .then(data => setWatches(data))
+  }, [])
+
+  // way 2: load data from '../public/watches.json'
+  // declare state
+  // const [watches, setWatches] = useState([])
+  // load data
+  // useEffect(() => {
+  //   fetch('watches.json')
+  //   .then(res => res.json())
+  //   .then(data => setWatches(data))
+  // }, [])
+
+  // way 1: creating fake data
+  // const watches = [
+  //   { id: 1, name: "Omega Speedmaster", price: 5500 },
+  //   { id: 2, name: "Rolex Submariner", price: 8500 },
+  //   { id: 3, name: "Tag Heuer Carrera", price: 3000 },
+  //   { id: 4, name: "Breitling Navitimer", price: 7200 },
+  //   { id: 5, name: "Seiko Prospex", price: 1200 }
+  // ]
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        watches.map(watch => <Watch key={watch.id} watch={watch}></Watch>)
+      }
     </>
   )
 }
--- es6-modules-overview/src/Components/Watch/Watch.jsx
+++ es6-modules-overview/src/Components/Watch/Watch.jsx
@@ -1,7 +1,10 @@
-const Watch = () => {
+const Watch = ({watch}) => {
+  const {name, price} = watch;
+
   return (
     <div>
-      
+      <h3>Watch: {name}</h3>
+      <p>Price: {price}</p>
     </div>
   );
 };
