666425f55b8e91c283fd04bbffdd279c69fe4bef
subas-roy <subasroy46@gmail.com>
2024-08-29 22:34:08 +0600

    Display Saved cart from Local storage

--- memorable-water-bottle/src/Components/Bottles/Bottles.jsx
+++ memorable-water-bottle/src/Components/Bottles/Bottles.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import './Bottles.css'
 import { addToLS, getStoredCart } from "../Utilities/localstorage";
+import Cart from "../Cart/Cart";
 
 const Bottles = () => {
   const [bottles, setBottles] = useState([]) // data load
@@ -15,10 +16,16 @@ const Bottles = () => {
 
   // load cart form local storage
   useEffect(() => {
-    console.log('call the stored cart', bottles.length)
     if (bottles.length > 0) {
       const storedCart = getStoredCart()
-      console.log(storedCart.length)
+      const savedCart = [];
+      for (const id of storedCart) {
+        const bottle = bottles.find(bottle => bottle.id == id);
+        if (bottle) {
+          savedCart.push(bottle)
+        }
+      }
+      setCart(savedCart)
     }
   }, [bottles])
 
@@ -31,7 +38,7 @@ const Bottles = () => {
   return (
     <div>
       <h3>Bottles Available: {bottles.length}</h3>
-      <h4>Cart: {cart.length}</h4>
+      <Cart cart={cart}></Cart>
       <div className="bottle-container">
       {
         bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle} handleAddToCart={handleAddToCart}></Bottle>)
