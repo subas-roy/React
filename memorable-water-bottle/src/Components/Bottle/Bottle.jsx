35e827a4c308f88e114312fdf4dae98415db3ead
subas-roy <subasroy46@gmail.com>
2024-08-29 10:09:38 +0600

    Local storage to store data

--- memorable-water-bottle/src/Components/Bottle/Bottle.jsx
+++ memorable-water-bottle/src/Components/Bottle/Bottle.jsx
@@ -1,12 +1,12 @@
 import './Bottle.css'
 
-const Bottle = ({bottle}) => {
-  console.log(bottle)
+const Bottle = ({bottle, handleAddToCart}) => {
   return (
     <div className="bottle">
       <h3>{bottle.name}</h3>
       <img src={bottle.img} alt="" />
-      <p>{bottle.price}</p>
+      <p>$ {bottle.price}</p>
+      <button onClick={handleAddToCart}>Purchage</button>
     </div>
   );
 };
--- memorable-water-bottle/src/Components/Bottles/Bottles.jsx
+++ memorable-water-bottle/src/Components/Bottles/Bottles.jsx
@@ -3,20 +3,27 @@ import Bottle from "../Bottle/Bottle";
 import './Bottles.css'
 
 const Bottles = () => {
-  // data load
-  const [bottles, setBottles] = useState([])
+  const [bottles, setBottles] = useState([]) // data load
+  const [cart, setCart] = useState([]) // cart state
+
   useState(() => {
     fetch('bottles.json')
     .then(res => res.json())
     .then(data => setBottles(data))
   }, [])
 
+  const handleAddToCart = bottle => {
+    const newCart = [...cart, bottle]
+    setCart(newCart)
+  }
+
   return (
     <div>
-      <h3>Bottles Here: {bottles.length}</h3>
+      <h3>Bottles Available: {bottles.length}</h3>
+      <h4>Cart: {cart.length}</h4>
       <div className="bottle-container">
       {
-        bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
+        bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle} handleAddToCart={handleAddToCart}></Bottle>)
       }
       </div>
     </div>
