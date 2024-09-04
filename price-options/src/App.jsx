56ea376cd1629aab032534d0a80a158e8c2f33b2
subas-roy <subasroy46@gmail.com>
2024-09-05 03:30:05 +0600

    Setup tailwind css with Create React App

---
+++ price-options/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-7xl'>Vite + React</h1>
+      <img
+      className="mask mask-heart"
+      src="https://img.daisyui.com/images/stock/photo-1567653418876-5bb0e566e1c2.webp" />
+    </>
+  )
+}
+
+export default App
