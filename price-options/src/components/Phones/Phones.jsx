7cece5d883100ddf90e432e67befa71696a6e834
subas-roy <subasroy46@gmail.com>
2024-09-07 12:03:12 +0600

    Explore React Awesome components

--- price-options/src/components/Phones/Phones.jsx
+++ price-options/src/components/Phones/Phones.jsx
@@ -1,11 +1,15 @@
 // import axios from "axios";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { BarChart, Bar, ResponsiveContainer, XAxis, YAxis, Tooltip } from 'recharts';
-
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
+import { Audio } from 'react-loader-spinner';
 
 const Phones = () => {
   const [phones, setPhones] = useState([]);
+
+  // state for loader / set loading true
+  const [loading, setLoading] = useState(true);
+
 
   useEffect(() => {
     // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -24,12 +28,24 @@ const Phones = () => {
       })
       console.log(phoneWithFakeData)
       setPhones(phoneWithFakeData)
+      // loading false
+      setLoading(false)
     })
 
   }, [])
 
   return (
     <div>
+        <h2>Loader/Spinner</h2>
+        {loading && <Audio
+          height="100"
+          width="100"
+          color="#4fa94d"
+          ariaLabel="audio-loading"
+          wrapperStyle={{}}
+          wrapperClass="wrapper-class"
+          visible={true}
+        />}
       <h2 className="text-5xl">Phones {phones.length}</h2>
       <BarChart width={1200} height={400} data={phones}>
         <Bar dataKey="price" fill="#8884d8" />
